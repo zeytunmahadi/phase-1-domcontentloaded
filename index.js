3f7a8126289f94cf2af651235f983c4c85be60b4
zeitun <zeytunmahadi718@gmail.com>
2024-05-29 08:40:17 +0300

    using a domcontentloaded to load html before javascript

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   const text = document.getElementById('text')
+   text.textContent ="This is really cool!"
+  });
